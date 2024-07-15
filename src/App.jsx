70f97aa27681031b417b3c6bc97231a27e39cb53
jegadish.am <jegadish.am@zohocorp.com>
2024-07-16 00:00:13 +0530

    Learning 1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
-import reactImg from "./assets/react-core-concepts.png";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom"/>
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header"
+import CoreConcept from "./components/CoreConcepts"
+import TabButton from "./components/TabButton";
 
 function App() {
+    let tabContent = "Please click a button";
+    function handleSelect(selectedButton) {
+        tabContent = selectedButton;
+    }
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+          <section id="core-concepts">
+            <h2>Time to get started!</h2>
+            <ul>
+                {
+                    CORE_CONCEPTS.map( coreConcept => {
+                            return <CoreConcept key={coreConcept.title} {...coreConcept} />
+                    })
+                }
+            </ul>
+          </section>
+          <section id="examples">
+              <h2>Examples</h2>
+              <menu>
+                <TabButton onSelect={() => handleSelect("components")}>Components</TabButton>
+                <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+                <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+                <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+              </menu>
+              {tabContent}
+          </section>
       </main>
     </div>
   );
